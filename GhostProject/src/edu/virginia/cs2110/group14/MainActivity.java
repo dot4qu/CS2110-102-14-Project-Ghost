940ddc7fda520e06f17662d7e0f385e9228aeb42
dot4qu <dot4qu@virginia.edu>
2014-10-29 10:27:21 -0400

    within git edit

--- GhostProject/src/edu/virginia/cs2110/group14/MainActivity.java
+++ GhostProject/src/edu/virginia/cs2110/group14/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//test edit
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
