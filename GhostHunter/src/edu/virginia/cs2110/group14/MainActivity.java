ed95258256bda346d1271c1b9ec48680d14539d2
dot4qu <dot4qu@virginia.edu>
2014-10-29 14:33:48 -0400

    Update MainActivity.java

--- GhostHunter/src/edu/virginia/cs2110/group14/MainActivity.java
+++ GhostHunter/src/edu/virginia/cs2110/group14/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//test
+		//test2
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
