49c4f35d6668b316e3723f1b1344f54e0411f12d
Brian Team <dot4qu@virginia.edu>
2014-11-05 23:03:12 -0500

    splash screen text formatting, forced landscape
    image is just a filler, not planning on actually using it

--- GhostHunter/src/edu/virginia/cs2110/group14/SplashScreen.java
+++ GhostHunter/src/edu/virginia/cs2110/group14/SplashScreen.java
@@ -5,16 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
+import android.widget.ImageView;
 
 
 public class SplashScreen extends Activity {
 
-	private int SPLASHSCREEN_LENGTH = 3000;
+	private int SPLASHSCREEN_LENGTH = 5000;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splashscreen);
+		
 		
 		new Handler().postDelayed(new Runnable() {
 		@Override
