6487c667b08dd9d970b0922c5d16d3a3b644b15f
bshin94 <bs5sk@virginia.edu>
2014-11-09 22:17:11 -0500

    Added Character
    Still need to incorporate buttons into this

--- GhostHunter/gen/edu/virginia/cs2110/group14/R.java
+++ GhostHunter/gen/edu/virginia/cs2110/group14/R.java
@@ -23,8 +23,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int ghost_logo=0x7f020000;
-        public static final int ghosthunter_background=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ghostbuster=0x7f020001;
+        public static final int ghosthunter_background=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
