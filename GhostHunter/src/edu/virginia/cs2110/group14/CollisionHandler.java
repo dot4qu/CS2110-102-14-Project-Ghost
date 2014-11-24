f74240ca872acd0f21a2df2f00ccd3cbdd001897
lwalter300 <ldw8fb@virginia.edu>
2014-11-23 22:19:44 -0500

    CollisionHandler class

---
+++ GhostHunter/src/edu/virginia/cs2110/group14/CollisionHandler.java
@@ -0,0 +1,24 @@
+package edu.virginia.cs2110.group14;
+
+import android.graphics.Rect;
+import android.widget.ImageView;
+
+public class CollisionHandler {
+
+ private Rect ghostBox;
+ private Rect busterBox;
+ 
+ public CollisionHandler(Ghost ghost, ImageView ghostBuster) {
+	 
+ ghostBox=new Rect();
+ ghost.getGhostImage().getDrawingRect(ghostBox);
+ ghostBuster.getDrawingRect(busterBox);
+ 
+}
+ 
+ public static void busterGhostCollision (Rect rect1, Rect rect2) {
+	 
+ }
+ 
+ 
+}
