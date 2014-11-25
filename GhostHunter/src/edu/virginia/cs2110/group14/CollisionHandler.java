cf7a40c0bdc7daf97fa85397f0ad513d42eb0123
lwalter300 <ldw8fb@virginia.edu>
2014-11-24 22:00:36 -0500

    Recommitting collision handler code.

--- GhostHunter/src/edu/virginia/cs2110/group14/CollisionHandler.java
+++ GhostHunter/src/edu/virginia/cs2110/group14/CollisionHandler.java
@@ -1,23 +1,53 @@
 package edu.virginia.cs2110.group14;
 
+import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.widget.ImageView;
 
 public class CollisionHandler {
 
- private Rect ghostBox;
- private Rect busterBox;
  
- public CollisionHandler(Ghost ghost, ImageView ghostBuster) {
- ghostBox=new Rect();
- ghost.getGhostImage().getDrawingRect(ghostBox);
+ public static String busterGhostCollisions (ImageView ghost, ImageView ghostBuster) {
+	 
+ Rect ghostBox=new Rect();
+ Rect busterBox=new Rect();
+ Rect bottomBound= new Rect();
+ Rect topBound=new Rect();
+ Rect leftBound=new Rect();
+ Rect rightBound=new Rect();
+ 
+ ghost.getDrawingRect(ghostBox);
  ghostBuster.getDrawingRect(busterBox);
  
-}
- 
- public static void busterGhostCollision (Rect rect1, Rect rect2) {
+ //handles ghost on buster collision
+ if (ghostBox.intersect(busterBox)) {
+	 if (ghostBox.centerX()<busterBox.centerX()) {
+		 return "ghost on left";
+	 } 
+	 if (ghostBox.centerX()>busterBox.centerX()) {
+		 return "ghost on right";
+	 }
+	 if (ghostBox.centerY()<busterBox.centerY()) {
+		 return "ghost on bottom=kill ghost";
+	 }
+	 if (ghostBox.centerY()>busterBox.centerY()) {
+		 return "ghost on top=kill buster";
+	 }
+	 
+//handles ghost on wall collision
+	 
+	 
+//handles buster on wall collision
 	 
  }
+return "no collision";
+}
+ 
+ 
+
  
  
 }
+
+// to call method: busterGhostCollision(ghost, hunter)
+//returns a string, and then use compare to to handle the collison
