259cc596b4cb44be0fb2248fa5da97a49bce38b4
lwalter300 <ldw8fb@virginia.edu>
2014-12-01 17:18:13 -0500

    Instructions

---
+++ GhostHunter/src/edu/virginia/cs2110/group14/Instructions.java
@@ -0,0 +1,15 @@
+package edu.virginia.cs2110.group14;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Instructions extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_instructions);
+	}
+}
